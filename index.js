2ccefeb063b91eb126cd2733af035e5c9517f825
Mwagiru <joymwagiru@gmail.com>
2022-03-06 22:41:48 +0300

    Added .js file and added content

---
+++ index.js
@@ -0,0 +1,17 @@
+function getBirthDateName(){
+    let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let Birthdate=document.getElementById("dob").value;
+    let Gender=document.getElementsByName("gender").value;
+    let birthday=new Date(Birthdate);
+    let weekdayName=birthday.getDay();
+    let maleAkanNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    let femaleAkanNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    if(Gender=="male"){
+        window.alert(birthday && maleAkanNames[weekdayName])
+    
+}else{  window.alert(birthday && femaleAkanNames[weekdayName])
+}
+if(Gender==""){
+    window.alert("Input Values!!");
+}
+}
