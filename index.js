323d7f7867aad4369994a6933e5a000f8797a22b
Mwagiru <99876386+Mwagiru@users.noreply.github.com>
2022-03-07 09:32:16 +0300

    Changes .js code

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 function getBirthDateName(){
     let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     let Birthdate=document.getElementById("dob").value;
-    let Gender=document.getElementsByName("gender").value;
+    let Gender=document.getElementsByName("gender");
     let birthday=new Date(Birthdate);
     let weekdayName=birthday.getDay();
     let maleAkanNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     let femaleAkanNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-    if(Gender==="male"){
+    if(male.checked){
         window.alert(birthday && maleAkanNames[weekdayName])
-    
-}else if(Gender==="female"){  window.alert(birthday && femaleAkanNames[weekdayName])
+}else if(female.checked){  window.alert(birthday && femaleAkanNames[weekdayName])
 }else{
-    window.alert("Input Values!!");
-}
-}
+    window.alert("Invalid!Please Choose gender and input Date of Birth")
+}}
